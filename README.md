a16fc012d46c2bf7e8a8889d5a615a62e9409335
PJunhyuk-mac <jgravity@naver.com>
2018-05-17 15:02:30 +0900

    Backup - 180517

--- README.md
+++
@@ -1,2 +0,0 @@
-# gsf2018-website
-Website of GSF2018(2018 Gangwon Startup Festival)
--- wp-content/plugins/newsletter/widget/standard.php
+++ wp-content/plugins/newsletter/widget/standard.php
@@ -18,7 +18,7 @@ class NewsletterWidget extends WP_Widget {
 
         $instance = array_merge(array('lists_layout' => '',
             'lists_empty_label' => '',
-            'lists_field_label' => ''), $instance);
+            'lists_label' => ''), $instance);
         
         $options_profile = get_option('newsletter_profile');
         //$form = NewsletterSubscription::instance()->get_form_javascript();
@@ -27,8 +27,8 @@ class NewsletterWidget extends WP_Widget {
         $form .= '<div class="tnp tnp-widget">';
         $form .= NewsletterSubscription::instance()->get_subscription_form_html5('widget', null, array(
             'list'=> implode(',', $instance['nl']),
-            'lists_layout' => $instance['lists_layout'],
-            'lists_empty_label' => $instance['lists_empty_label'],
+            'lists_field_layout' => $instance['lists_layout'],
+            'lists_field_empty_label' => $instance['lists_empty_label'],
             'lists_field_label' => $instance['lists_field_label'],
             
             ));
